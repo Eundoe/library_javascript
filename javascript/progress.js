2452d7de43ed008100780b0c1104af583381d010
Bel Cho <eundoe92@gmail.com>
2024-02-10 23:05:08 +0900

    UPDATED 2024.02.10 첫 업뎃

---
+++ javascript/progress.js
@@ -0,0 +1,76 @@
+//전처리 또는 후처리에 사용되는 함수
+
+
+/**HTML을 MSWORD로 변환시켜 다운 또는 열람하는 함수 해당기능은 Chrome에서만 작동한다.
+ * 사용할때 주의점으론 비동기성 함수를 사용하므로 반드시 그려져있는 대상만을 
+ * 변환해서 출력해올수있다. 이때 그려지는 대상을 비시각화 시킴으로서 최소화시키는게 필요하다.
+ * HTML에서 MSWORD로 호환할때는 style태그에서는 거의 대부분의 기능이 작용하지 않는다 필요하다면 
+ * 해당 태그에서 직접 style attribute를 이용해서 작성해야만 적용된다.
+ * 몇몇 html-msword 상호간 적용가능 style property에대한 서술은 하기에 따른다 
+ * font-size는 html font-size와 다르게 적용된다(html에 적용된 폰트의 약 -3.5px ~ -4px 정도)
+ * font-weight는 bolder만 적용 가능
+ * line-height는 percent 타입만 적용가능
+ * text-align은 left/center/right만 적용가능
+ * 줄바꿈은 <br/>을 사용할것
+@param {String} element_id 변환할 부분의 전체를 wrap하고 있는 element의 id
+@param {String} file_name 변환하여 다운받을때 
+@version 0.0.0 UPDATE 2024.02.10
+@author 조재호 <eundoe92@gmail.com>
+*/
+function convertHTMLtoMSWord(element_id, file_name){
+    const new_html = `
+    <html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'>
+        <head>
+          <meta charset='utf-8'>
+          <title>Export HTML To Doc</title>
+          <style>
+          div{
+            font-size:16px;
+          }
+          </style>
+        </head>
+        <body>
+          ${document.getElementById(String(element_id)).innerHTML}
+        </body>
+    </html>
+    `
+    const html_blob = new Blob(['\ufeff',new_html],{type:'application/msword'})
+    const encode_url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(new_html)
+    const doc_name = String(file_name) + '.doc'
+    const download_btn = document.createElement('a')
+    document.body.appendChild(download_btn)
+
+    // only use in chrome
+    if(navigator.msSaveOrOpenBlob){
+        navigator.msSaveOrOpenBlob(html_blob,doc_name)
+    }else{
+        download_btn.href = encode_url
+        download_btn.download = doc_name
+        download_btn.click()
+    }
+    document.body.removeChild(download_btn)
+}
+
+
+/**자릿수 추가하는 함수 받아온 숫자데이터를 가지고 필요한만큼 0을 추가하기위한 함수
+ * 앞의 자릿수가 커지면 커질수록 추가되는 데이터는 따로 존재하지 않게됨
+@param {String | Number} number 받아오는 숫자데이터 숫자형도 문자형도 전부 문자형으로 변환해서 검증함
+@param {Number} digit 자릿수를 얼마로 할것인지 설정하는 부분 오직 숫자만 받아와야함
+@version 0.0.0 UPDATE 2024.02.10
+@return {String} 자릿수를 가진 데이터 String 값으로 변환 만약 변수가 검증을 통과하지못한다면 기존데이터를 String으로 반환
+@author 조재호 <eundoe92@gmail.com>
+*/
+function convertNumberDigit(number, digit){
+    const number_regex = /[0-9]/
+    if(number_regex.test(String(number)) && typeof digit === 'number' && digit !== null && digit > 1){
+        const number_length = String(number).length
+        let new_digit = ''
+        for(let index = 0; index < digit - number_length; index++){
+            new_digit += '0'
+        }
+        new_digit += String(number)
+        return new_digit
+    }else{
+        return String(number)
+    }
+}
---
+++ javascript/validation.js
@@ -0,0 +1,110 @@
+// 유효성 검사용 함수들
+
+/**국내 핸드폰번호 유효성검사 13자리 11자리 하이픈포함
+@param {String | Number} mobile_number 검증하려는 모바일 번호 데이터
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean} True/Flase
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validateMobileKr(mobile_number){
+ const mobile_regex_kr_13 = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+ const mobile_regex_kr_11 = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/;
+ if(String(mobile_number).length <= 11){
+    return mobile_regex_kr_11.test(String(mobile_number));
+ }else if(String(mobile_number).length <=13){
+    return mobile_regex_kr_13.test(String(mobile_number));
+ }else{
+    return false;
+ }
+}
+
+/**국제 전화번호 유효성 검사 length상관없이 모든 전화번호를 하이픈포함해서 검증
+@param {String | Number} phone_number 검증하려는 전화번호 데이터
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean} True/False
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validatePhone(phone_number){
+ const phone_regex = /^[0-9][0-9\-]*[0-9]$/;
+ return phone_regex.test(String(phone_number));
+}
+
+
+/**국내 한국어 유효성 검사 확인 검증데이터 결과를 전체(all), 단어기준(word), 모음자음기준("char")
+ * 로 나눠서 결과를 출력 해당 결과값에서 필요한 내용만 가져다가 사용.
+@param {String} korean_char 검증하려는 문자데이터
+@param {Boolean} detail 상세데이터를 받는경우 체크
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean}  detail이 False거나 없을경우 출력 True/False
+@return {Object}  detail이 True인경우 {all: Boolean, word: Boolean, char: Boolean}
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validateKoreanChar(korean_char, detail){
+    const korean_regex_all = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    const korean_regex_word = /[가-힣]/;
+    const korean_regex_char = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
+    if(!detail || detail === false){
+        return korean_regex_all.test(korean_char);
+    }
+
+    if(detail === true){
+        const result = {
+            all: korean_regex_all.test(korean_char),
+            word: korean_regex_word.test(korean_char),
+            char : korean_regex_char.test(korean_char),
+        }
+        return result;
+    }
+}
+
+
+function validateEnglishChar(english_char){
+    const english_regex = /[a-zA-Z]/
+    return english_char.test(english_regex)
+}
+
+
+/**국제 RFC5322방식 이메일 유효성 체크용 함수 거의 모든 이메일을 통과시키기 때문에 이메일형식 체크용으로사용
+ * 특정 이메일 체크를 사용할경우 validateEmailSingle함수를 병용해서 사용해야함
+@param {String} email 검증하려는 이메일 데이터
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean} True/False
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validateEmailAll(email){
+    const rfc5322_regex =  new RegExp(
+        "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])",
+      );
+    return rfc5322_regex.test(String(email))
+}
+
+/**엄격한 도메인체크가 필요할경우 추가로 사용해야하는 유효성검사함수 regex형식으로 입력해도 작동하고 그냥 입력해도 작동하기때문에
+ * 필요한 도메인체크를 할것
+@param {String} email 검증할 이메일 데이터
+@param {String} domain 추가로 검증할 도메인 데이터
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean} True/False 도메인이없거나 빈칸일경우 False 반환
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validateEmailSingle(email, domain){
+    const domain_regex = new RegExp('[a-zA-Z0-9]+@' + String(domain))
+    if(!domain || String(domain).trim().replace(/\s/g,'') === ''){
+        return false
+    }
+    return domain_regex.test(String(email))
+}
+
+/**컨텐츠가 빈데이터인지 확인하는 유효성검사 앞뒤공간과 중간공간을 전부 체크해서 
+ * 빈칸을 전부 매꾼뒤 체크함 체크한 결과값에 따라 반환
+@param {String} contents 유효성 검증 체크할 데이터
+@version 0.0.0 UPDATE 2024.02.10
+@return {Boolean} True/False 데이터가 없을경우 False 
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validateBlank(contents){
+    if(!contents){
+        return false
+    }
+    const progressed_contents = String(contents).trim().replace(/\s/g,'');
+    return progressed_contents === '' ?  false : true;
+}
