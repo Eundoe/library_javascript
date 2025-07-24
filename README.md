c976fc919132bcd379a2a2f097a4a24b240cfbdc
Jaeho.Cho <eundoe92@gmail.com>
2025-07-24 09:41:05 +0900

    feat: 수정사항 변경

--- README.md
+++ README.md
@@ -1,23 +1,45 @@
 # 자주쓰는 기능 종합
 
 ## Summary
+
 - 자주쓰는 함수들을 매번 구글링하기 귀찮고 특수한 경우에 리팩토링이 필요한 경우가 많았는데 불편해서 모아두기위한 함수덩어리입니다.
 - 라이브러리화 시키기에는 특정 기능만 써야하는경우도 있고 특정 데이터만 써야하는경우도 있는데다가 코드검증하기도 귀찮아서 검증된 코드들을 모아서합니다.
 - 혹시라도 가져다가 사용하실떄는 서비스에 맞는지 한번더 고민해보고 체크해보고 그다음에 사용하시길 바랍니다.
 - 잘하는 사람이 아니기 때문에 코드에 오류가 있을수도 있고 비효율적인 부분도 있을수 있습니다. 의견은 언제나 환영입니다.
 
-## Validation 
+## Validate
+
 - 유효성관련 데이터를 모아놓은 함수
 - 유효성검사 추가 2025.04.10
+- 유효성검사 추가 및 파일명변경 (Validation -> Validate)로 변경 2025.07.24
 
 ## Progress
+
 - 전처리 또는 후처리에 필요한 함수
 - Timezone 포함한 UTC추가 2025.04.10
 
 ## Event
-- 이벤트 액션 관리 처리 함수 
 
-## Static Data 
-- 자주 사용할수도 있는 데이터들 모음집 
+- 이벤트 액션 관리 처리 함수
+
+## Static Data
+
+- 자주 사용할수도 있는 데이터들 모음집
 - country 추가 2024.02.10
 - phone_info 추가 2025.04.10
+
+### .vscode
+
+```
+{
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  "explorer.confirmDragAndDrop": true,
+  "prettier.singleQuote": true,
+  "editor.suggestSelection": "first",
+  "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+  "editor.formatOnSave": true,
+  "window.zoomLevel": 0.5,
+  "editor.defaultFormatter": "esbenp.prettier-vscode"
+}
+
+```
---
+++ javascript/validate.js
@@ -0,0 +1,134 @@
+// Validation 목적의 유틸 함수모음 실패시 유효성에 부합하지않을시 False반환 부합할시 True반환(고정)
+
+/**컨텐츠가 빈데이터인지 확인하는 유효성검사 앞뒤공간과 중간공간을 전부 체크해서 
+ * 빈칸을 전부 매꾼뒤 체크함 체크한 결과값에 따라 반환
+@param {String | Number} v 유효성 검증 체크할 데이터
+@version 0.0.1 UPDATE 2025.07.24 변수값일관화 데이터 검증 가능여부 확인필요
+@return {Boolean} True(값없음)/False(값있음)
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validEmpty(v) {
+  if (typeof v !== 'string' && typeof v !== 'number') {
+    return false;
+  }
+  return String(v).replace(/\s/g, '') === '';
+}
+
+/** 컨텐츠에 이모지가 포함되어있는지 확인 
+  @param {String} v 체크할 단어 문장 또는 항목
+  @version 0.0.1 2025-07-24 함수명 변경획일화
+  @return {Boolean} True(이모지포함)/False(이모지미포함/타입이안맞음)
+  @author 조재호 <eundoe92@gmail.com>
+  */
+function validEmoji(v) {
+  if (typeof v != 'string') {
+    return false;
+  }
+  return /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/.test(
+    v
+  );
+}
+
+/** 디바이스 체크용 함수
+@version 0.0.1 UPDATED 2025-07-24 함수명 변경 획일화
+@return {Boolean} True(모바일)/False(데스크탑)
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validMobile() {
+  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+    navigator.userAgent
+  );
+}
+
+/**대한민국기준 국내핸드폰번호 유효성검사
+@param {String} v 검증하려는 휴대번호 데이터
+@version 0.0.1 UPDATE 2025.07.24 함수명변경
+@return {Boolean} True(포맷에맞음)/False(포맷X|타입안맞음)
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validCellPhoneDom(v) {
+  if (typeof v != 'string') {
+    return false;
+  }
+  return /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/.test(v);
+}
+
+/**컨텐츠가 URL 도메읺 형식에 합당한 데이터인지 검증하는 함수
+ * 검증시에 http:// https://를 둘다 검증해준다.
+@param {String} v 검증할 도메인값
+@version 0.0.1 UPDATE 2025.07.24 함수명 변경 검증 추가
+@returns {Boolean} True(Url O)/False(Url X/타입다름)
+@author 조재호 <eundoe92@gmail.com>
+*/
+function validWebUrl(v) {
+  if (typeof v !== 'string') {
+    return false;
+  }
+  return /(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/gi.test(
+    v
+  );
+}
+
+/** 이메일 유효성 검증 함수
+@param {String} v 검증할 이메일값
+@param {String[]} domains 검증할 도메인 Array로 추가
+@param {Boolean} strict rfc5322체크 기본값 false
+@version 0.0.1 UPDATED 2025.07.24 
+@return {Boolean} True(도메인통과)/False(도메인실패)
+*/
+
+function validEmail(v, domains = [], strict = false) {
+  if (typeof v !== 'string' || !Array.isArray(domains)) {
+    return false;
+  }
+  const defaultMail =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const rfc5322 = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+
+  if (!defaultMail.test(v)) {
+    return false;
+  }
+  if (strict && !rfc5322.test(v)) {
+    return false;
+  }
+  if (domains.length > 0) {
+    const domainPart = v.split('@')[1].toLowerCase();
+    for (let i = 0; i < domains.length; i++) {
+      if (typeof domains[i] !== 'string') {
+        continue;
+      }
+      const cleanDomain = domains[i].replace('@', '').toLowerCase();
+      if (domainPart === cleanDomain) {
+        return true;
+      }
+    }
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/** 두 값의 데이터 비교
+@param {String|Number|Boolean|BigInt} vi 파라미터설정
+@param {String|Number|Boolean|BigInt} vii 파라미터설정
+@version 0.0.1 UPDATED 2025.07.24
+@return {Boolean} True(두값과 타입이 일치)/False(값또는 타입이 일치안함)
+*/
+function validEqualValue(vi, vii) {
+  const viType = typeof vi;
+  if (
+    viType !== typeof vii ||
+    (viType !== 'string' &&
+      viType !== 'number' &&
+      viType !== 'boolean' &&
+      viType !== 'bigint')
+  ) {
+    return false;
+  }
+  if (Number.isNaN(vi) || Number.isNaN(vii)) {
+    return false;
+  }
+  return vi === vii;
+}
